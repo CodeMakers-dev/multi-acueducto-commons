2c84ad300cf17547702ba7c814c0dc6c9be154b0
DESKTOP-DL3TM1L\ADMIN <nicope2004@gmail.com>
2025-05-27 11:05:29 -0500

    First Commit

---
+++ src/main/java/com/codemakers/commons/MultiAcueductoCommonApplication.java
@@ -0,0 +1,13 @@
+package com.codemakers.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAcueductoCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiAcueductoCommonApplication.class, args);
+	}
+
+}
