09112484d0d85021f241057624cbc55d86420441
DESKTOP-DL3TM1L\ADMIN <nicope2004@gmail.com>
2025-06-11 14:28:55 -0500

    Ajustes clase constantes

--- src/main/java/com/codemakers/commons/utils/Constantes.java
+++ src/main/java/com/codemakers/commons/utils/Constantes.java
@@ -1,18 +1,24 @@
 package com.codemakers.commons.utils;
 
 public class Constantes {
+	
+	private Constantes() {
+        throw new UnsupportedOperationException("Esta clase no puede ser instanciada");
+    }
 
 	public static final String SAVED_SUCCESSFULLY ="Guardado exitosamente";
 	public static final String UPDATED_SUCCESSFULLY ="Actualizado exitosamente";
 	public static final String DELETED_SUCCESSFULLY ="Eliminado exitosamente";
 	public static final String CONSULTED_SUCCESSFULLY ="Consulta exitosamente";
 	public static final String SENT_SUCCESSFULLY ="Enviado exitosamente";
+	public static final String AUTHENTICATION_SUCCESSFULLY ="Autenticación exitosa";
 	public static final String SEND_ERROR ="Error al enviar mensaje";
 	
 	public static final String SAVE_ERROR = "Error al guardar";
 	public static final String PASSWORD_SUCCESSFULLY ="Contraseña actualizada correctamente.";
 	public static final String UPDATE_ERROR = "Error al actualizar";
 	public static final String DELETE_ERROR = "Error al eliminar, registro no encontrado";
+	public static final String AUTHENTICATION_ERROR ="Error inesperado al autenticar";
 	public static final String RECORD_NOT_FOUND = "Registro no encontrado";
 	public static final String CONSULTING_ERROR = "Error al consultar";
 	public static final String ERROR_QUERY_RECORD_BY_ID = "Error al consultar registro por Id";
@@ -28,5 +34,6 @@ public class Constantes {
 	public static final String USER_INVALID = "El usuario está inactivo. No se puede recuperar la contraseña.";
 	
 	public static final String INVALID_CREDENTIALS = "Credenciales inválidas";
+	public static final String INTERNAL_SERVER_ERROR = "Error interno del servidor";
 
 }
